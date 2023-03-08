40f1039afffa62595e394193cdac5ac8d04c442b
Mohan <umohanbabu107@gmail.com>
2023-03-08 15:46:56 +0530

    Second commit from Mohan1

--- src/pkgGitPractice/Programme1.java
+++ src/pkgGitPractice/Programme1.java
@@ -3,5 +3,6 @@ package pkgGitPractice;
 public class Programme1 {
 	public static void main(String[] args) {
 		System.out.println("Hi this is Mohan and I have added this code");
+		System.out.println("Hi this is Mohan2 and I have added this");
 	}
 }
