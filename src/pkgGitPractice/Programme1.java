2097086d7bbc78878900e4e07698c04e54ae1cc7
Mohan <umohanbabu107@gmail.com>
2023-03-08 18:19:20 +0530

    Mohan added this commit

--- src/pkgGitPractice/Programme1.java
+++ src/pkgGitPractice/Programme1.java
@@ -4,5 +4,7 @@ public class Programme1 {
 	public static void main(String[] args) {
 		System.out.println("Hi this is Mohan and I have added this code");
 		System.out.println("Hi this is Mohan2 and I have added this");
+		// Below code is done by the Mohan
+		System.out.println("This line is written by Mohan in GitPractice-M1 project");
 	}
 }
